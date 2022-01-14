d69a327ef03f034263619ea83f26e4857ee01081
arju10 <arjuarju71@gmail.com>
2022-01-14 15:09:15 +0600

    mongodb connected

--- server.js
+++ server.js
@@ -2,21 +2,13 @@ const app = require("./app.js");
 const dotenv = require("dotenv");
 
 // Config
-
 dotenv.config({path:"./config/config.env"});
 
+// Connecting to Database
+const connectDatabase = require("./config/database");
+connectDatabase();
 
-// Connect Mongodb
-mongoose.connect(process.env.DB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-}).then((data) => {
-    console.log(`Mongodb is connected with server successfully ${data.connection.host}`);
-}).catch((err) => {
-    console.log(err);
-})
+
 
 
 app.listen(process.env.PORT, ()=> {
